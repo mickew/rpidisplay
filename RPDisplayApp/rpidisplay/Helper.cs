fccf8ec9df4400c3097e2e60fffbe8de01791a7d
Mikael Wågberg <mikael_wagberg@trimble.com>
2025-03-04 10:45:32 +0100

    Add rpidisplay project with system info retrieval
    Updated solution to include a new project "rpidisplay" with configurations for Debug and Release modes. Added a `Helper` class in `Helper.cs` to retrieve system information (IP address, CPU load, memory usage, disk usage) using Linux commands. Modified `Program.cs` to display this information on an I2C-connected display. Included a new logo image `logo.bmp` and created the project file `rpidisplay.csproj` with necessary settings and package references.

---
+++ RPDisplayApp/rpidisplay/Helper.cs
@@ -0,0 +1,100 @@
+﻿using System.Diagnostics;
+
+namespace rpidisplay;
+
+public static class Helper
+{
+    public static string GetIpAddress()
+    {
+        var fileName = "/bin/bash"; //linux command i want to execute
+        var arguments = "-c \"hostname -I |cut -f 1 -d ' '\"";  //the argument to return ip address
+        using Process proc = CreateProcess(fileName, arguments);
+        string ss = string.Empty;
+
+        proc.Start();  //start the process
+        while (!proc.StandardOutput.EndOfStream)  //wait until entire stream from output read in
+        {
+            ss = $"{ss}{proc.StandardOutput.ReadLine()}";  //this contains the output                    
+        }
+        return $"IP: {ss}";
+    }
+
+    public static string GetCpuLoad()
+    {
+        var fileName = "/bin/bash"; //linux command i want to execute
+        var arguments = "-c \"top -bn1\"";  //the argument to return cpu load
+        using Process proc = CreateProcess(fileName, arguments);
+        var list = new List<string>();
+        string ss = string.Empty;
+
+        proc.Start();  //start the process
+        while (!proc.StandardOutput.EndOfStream)  //wait until entire stream from output read in
+        {
+            list.Add(proc.StandardOutput.ReadLine()!);
+            //ss = $"{ss}{proc.StandardOutput.ReadLine()}";  //this contains the output                    
+        }
+        ss = list.First().ToString();
+        //top - 00:34:19 up  6:20,  1 user,  load average: 0.21, 0.24, 0.23
+        var i = ss.LastIndexOf(':');
+        ss = ss.Substring(i + 1).Split(',')[0].Trim();
+        return $"CPU Load: {ss}";
+    }
+
+    public static string GetMemoryUsage()
+    {
+        var fileName = "/bin/bash"; //linux command i want to execute
+        var arguments = "-c \"free -m\" | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'";  //the argument to return memory usage
+        using Process proc = CreateProcess(fileName, arguments);
+        var list = new List<string>();
+        string ss = string.Empty;
+
+        proc.Start();  //start the process
+        while (!proc.StandardOutput.EndOfStream)  //wait until entire stream from output read in
+        {
+            list.Add(proc.StandardOutput.ReadLine()!);
+            //ss = $"{ss}{proc.StandardOutput.ReadLine()}";  //this contains the ip output                    
+        }
+        var sTotal = list[1].Split("       ")[1].Trim();
+        var sUsed = list[1].Split("       ")[2].Trim();
+        var percentageused = double.Parse(sUsed) / double.Parse(sTotal) * 100;
+        ss = $"Mem: {sUsed}/{sTotal}MB {percentageused.ToString("#.0")}%";
+        return ss;
+    }
+
+    public static string GetDiskUsage()
+    {
+        var fileName = "/bin/bash"; //linux command i want to execute
+        var arguments = "-c \"df -h\" | awk '$NF==\"/\"{printf \"Disk: %d/%d GB %s\", $3,$2,$5}'";  //the argument to return disk usage
+        using Process proc = CreateProcess(fileName, arguments);
+        var list = new List<string>();
+        string ss = string.Empty;
+
+        proc.Start();  //start the process
+        while (!proc.StandardOutput.EndOfStream)  //wait until entire stream from output read in
+        {
+            list.Add(proc.StandardOutput.ReadLine()!);
+            //ss = $"{ss}{proc.StandardOutput.ReadLine()}";  //this contains the ip output                    
+        }
+        var sZise = list[1].Split("  ")[3].Trim().TrimEnd('G');
+        var sUsed = list[1].Split("  ")[4].Trim().TrimEnd('G');
+        var sPercentageUsed = list[1].Split("  ")[6].Trim().TrimEnd('/');
+        ss = $"Disk: {sUsed}/{sZise}GB {sPercentageUsed}";
+        return ss;
+    }
+
+    private static Process CreateProcess(string fileName, string arguments)
+    {
+        var proc = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = fileName,
+                Arguments = arguments,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,  //redirect output to my code here
+                CreateNoWindow = true //do not show a window
+            }
+        };
+        return proc;
+    }
+}
---
+++ RPDisplayApp/rpidisplay/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Device.I2c;
+using System.Drawing;
+using Iot.Device.Graphics;
+using Iot.Device.Graphics.SkiaSharpAdapter;
+using Iot.Device.Ssd13xx;
+
+namespace rpidisplay;
+
+internal class Program
+{
+    private const string Font = "DejaVu Sans";
+    private const int FontSize = 8;
+    private const int DisplayInfoTimeout = 1000;
+    private const int DisplayLogoTimeout = 15000;
+    private static bool keepRunning = true;
+
+    static void Main(string[] args)
+    {
+        Console.CancelKeyPress += delegate (object? sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+            keepRunning = false;
+        };
+        Console.WriteLine("Starting...");
+
+        SkiaSharpAdapter.Register();
+        I2cConnectionSettings connectionSettings = new(1, 0x3C);
+        I2cDevice? i2cDevice = I2cDevice.Create(connectionSettings);
+        Ssd13xx device = new Ssd1306(i2cDevice, 128, 32);
+
+        device.ClearScreen();
+        DisplayImage(device);
+        device.ClearScreen();
+
+        while (keepRunning)
+        {
+            var ipAddress = Helper.GetIpAddress();
+            var cpuLoad = Helper.GetCpuLoad();
+            var memoryUsage = Helper.GetMemoryUsage();
+            var diskUsage = Helper.GetDiskUsage();
+            DisplayInfo(device, Font, FontSize, ipAddress, cpuLoad, memoryUsage, diskUsage);
+        }
+
+        DisplayImage(device, false);
+        device.Dispose();
+        Console.WriteLine("Shuting down...");
+    }
+
+    private static void DisplayInfo(GraphicDisplay ssd1306, string font, int fontSize, string ipAddress, string cpuLoad, string memoryUsage, string diskUsage)
+    {
+        var y = 0;
+        using (var image = BitmapImage.CreateBitmap(128, 32, PixelFormat.Format32bppArgb))
+        {
+            image.Clear(Color.Black);
+            var g = image.GetDrawingApi();
+            g.DrawText(ipAddress, font, fontSize, Color.White, new Point(0, y));
+            y += (fontSize - 1);
+            g.DrawText(cpuLoad, font, fontSize, Color.White, new Point(0, y));
+            y += (fontSize - 1);
+            g.DrawText(memoryUsage, font, fontSize, Color.White, new Point(0, y));
+            y += (fontSize - 1);
+            g.DrawText(diskUsage, font, fontSize, Color.White, new Point(0, y));
+            ssd1306.DrawBitmap(image);
+        }
+    }
+
+    private static void DisplayImage(GraphicDisplay ssd1306, bool waitForTimeout = true)
+    {
+        Console.WriteLine("Display Image");
+        var image_name = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "images", "logo.bmp");
+        using BitmapImage image = BitmapImage.CreateFromFile(image_name);
+        ssd1306.DrawBitmap(image);
+        if (waitForTimeout)
+        {
+            Thread.Sleep(DisplayLogoTimeout);
+        }
+    }
+}
